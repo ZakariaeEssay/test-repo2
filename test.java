fe944d8fd0c74c4474e600972229073cdffe1e64
ZakariaeEssay <108145153+ZakariaeEssay@users.noreply.github.com>
2022-06-25 12:52:52 +0100

    Create test.java

---
+++ test.java
@@ -0,0 +1,22 @@
+package ma.ensah;
+/*
+
+hey there i add same commnets here 
+
+*/
+
+//classe test
+public class test {
+    //the main method it's the entry point in the program
+    public static void main(String[] args) {
+
+        //print hello to the user
+        System.out.println("hello");
+
+        //TODO...
+
+        //bye to the user
+        System.out.println("bye!!!");
+
+    }
+}
